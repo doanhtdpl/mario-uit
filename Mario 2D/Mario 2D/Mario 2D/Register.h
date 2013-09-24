d895d5321d8d786554e3e3947e6262983d8fb2a4
promiennam <promiennam@3598aa68-a51b-df3c-f868-cbe25ef59483>
2013-09-24 11:53:03 +0000

    - for modify framework (adjust & add some class)

--- Mario 2D/Mario 2D/Mario 2D/Register.h
+++ Mario 2D/Mario 2D/Mario 2D/Register.h
@@ -5,15 +5,15 @@
 #include <d3d9.h>
 #include <d3dx9.h>
 
-#define NameClass "@========{==== Mario 2D >> Copyright by Philosopher ft Simba << UIT ======>"
+#define NameClass "Mario 2D << UIT >> Copyright by Philosopher ft Simba"
 #define SCREEN_WIDTH	800 // screen width
 #define SCREEN_HEIGHT	600 // screen height
 
 class CRegister
 {
 private:
-	HINSTANCE m_hInstance;	// instance handler
-	HWND m_wndHandle;		// windows handler
+	HINSTANCE	m_hInstance;	// instance handler
+	HWND		m_wndHandle;	// windows handler
 public:
 	CRegister(void);
 	~CRegister(void);
@@ -21,8 +21,8 @@ public:
 
 	bool InitWindows();	// Init windows
 
-	HINSTANCE Get_hInstance(); // Get instance handler
-	HWND Get_WndHandle();	// Get windows handler
+	HINSTANCE Get_hInstance();	// Get instance handler
+	HWND Get_WndHandle();		// Get windows handler
 
 	static LRESULT CALLBACK WndProc(HWND,UINT,WPARAM,LPARAM); // Windows Processing
 };
