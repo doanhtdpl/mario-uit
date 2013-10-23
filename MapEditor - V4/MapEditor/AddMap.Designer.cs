8e933d36486cf6c9769b7ba8e70b2b8308763adb
tuanh0501 <tuanh0501@gmail.com>
2013-10-23 12:29:42 +0000

    + for Add Map Editor

---
+++ MapEditor - V4/MapEditor/AddMap.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace MapEditor
+{
+    partial class AddMap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRow = new System.Windows.Forms.TextBox();
+            this.txtColum = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Row";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Colum";
+            // 
+            // txtRow
+            // 
+            this.txtRow.Location = new System.Drawing.Point(69, 83);
+            this.txtRow.Name = "txtRow";
+            this.txtRow.Size = new System.Drawing.Size(100, 20);
+            this.txtRow.TabIndex = 3;
+            this.txtRow.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtColum_KeyPress);
+            // 
+            // txtColum
+            // 
+            this.txtColum.Location = new System.Drawing.Point(69, 45);
+            this.txtColum.Name = "txtColum";
+            this.txtColum.Size = new System.Drawing.Size(100, 20);
+            this.txtColum.TabIndex = 4;
+            this.txtColum.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtColum_KeyPress);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(187, 46);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(187, 80);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 7;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // AddMap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 147);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtRow);
+            this.Controls.Add(this.txtColum);
+            this.Name = "AddMap";
+            this.Text = "AddMap";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRow;
+        private System.Windows.Forms.TextBox txtColum;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
---
+++ MapEditor - V4/MapEditor/MapEditor_Class/Map_Config.cs
@@ -0,0 +1,31 @@
+﻿/* 
+File lưu trữ các biến để tùy chỉnh 
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace MapEditor
+{ 
+    public class Map_Const
+    {
+        public static int imgSelect = 0;
+        //Xác lập chiều dài và rộng đối tượng trên Map
+        public static int Height = 40;
+        public static int Width = 40;
+        //xac dinh chieu dai cua map
+        public static int Map_Height = 0;
+        public static int Map_Width = 0;
+        //Thiết lập màu
+        public static Color Grid_Color = Color.White;
+        public static Color Background = Color.FromArgb(65, 128, 190);
+        //Số lượng phần tử trong ma trận Matrix[y,x]
+        public static int X;
+        public static int Y;
+        //Phần tử mặc định ban đầu của map
+        public static int Default = 0;
+    }
+}
