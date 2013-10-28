deec4bf30efe1aa31250c5eaa80e5b073cd76ec4
tuanh0501 <tuanh0501@gmail.com>
2013-10-28 16:01:44 +0000

    + Mapeditor Version 0.5

---
+++ MapEditor - V0.5/MapEditor/AddMap.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MapEditor
+{
+    public partial class AddMap : Form
+    {
+        public int cols = 0, rows = 0;
+        public AddMap()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cols = Int32.Parse(txtColum.Text);
+                rows = Int32.Parse(txtRow.Text);
+                this.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Error type number");
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtColum_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar))
+                e.Handled = true;
+        }
+    }
+}
---
+++ MapEditor - V0.5/MapEditor/NewMap.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MapEditor
+{
+    public partial class NewMap : Form
+    {
+        public int cols, rows;
+        
+        public NewMap()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cols = Int32.Parse(txtColum.Text);
+                rows = Int32.Parse(txtRow.Text);
+                this.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Error type number");
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            cols = Map_Const.X;
+            rows = Map_Const.Y;
+            this.Close();
+        }
+    }
+}
